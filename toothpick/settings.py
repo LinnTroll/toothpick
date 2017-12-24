2ea931632bb5e435f095f2a228576d3cc0182e54
linntroll <pavel.hoodoleev@gmail.com>
2017-12-25 00:20:14 +0200

    database settings for heroku

--- toothpick/settings.py
+++ toothpick/settings.py
@@ -76,13 +76,12 @@ WSGI_APPLICATION = 'toothpick.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+import dj_database_url
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(default='sqlite:///db.sqlite3')
 }
 
+print (DATABASES)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
