3a5158b23e5466ac038464a41793a5423880b686
linntroll <pavel.hoodoleev@gmail.com>
2017-12-24 19:32:48 +0200

    add amdin management for toothpick

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from core.models import Toothpick, ToothpickOwnership
+
+
+class ToothpickOwnershipInline(admin.TabularInline):
+    model = ToothpickOwnership
+    readonly_fields = ('user', 'enabled', 'own_start_at', 'own_end_at')
+    extra = 0
+    max_num = 0
+    can_delete = False
+
+
+class ToothpickAdmin(admin.ModelAdmin):
+    def get_owners(self, item):
+        return [u.username for u in item.actual_owners()]
+
+    get_owners.short_description = 'owners'
+    list_display = ('name', 'get_owners')
+    inlines = (ToothpickOwnershipInline,)
+
+
+admin.site.register(Toothpick, ToothpickAdmin)
